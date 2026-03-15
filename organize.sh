d1215a5bd6fa926494f4c634131a774281c13c0f
IliasSari <sarigilias@gmail.com>
2026-03-15 17:34:49 +0200

    Add cleanup logic

--- organize.sh
+++ organize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-LOG_FILE="organize.log"
 
+LOG_FILE="organize.log"
 IMG_DIR="Photos"
 DOC_DIR="Documents"
 TXT_DIR="PlainTexts"
@@ -14,17 +14,19 @@ echo "Starting organization..."
 
 if ls *.jpg *.png *.jpeg >/dev/null 2>&1; then
     mv *.jpg *.png *.jpeg $IMG_DIR/
-    echo "✅ The images were transferred to $IMG_DIR." >> $LOG_FILE
+    echo "[$(date +%H:%M:%S)] ✅ The images were transferred to $IMG_DIR." >> $LOG_FILE
 else
-    echo "ℹ️ No new images found." >> $LOG_FILE
+    echo "[$(date +%H:%M:%S)]ℹ️ No new images found." >> $LOG_FILE
 fi
 
 if ls *.pdf *.docx >/dev/null 2>&1; then
-    mv *.pdf *.docx Documents/
+    mv *.pdf *.docx $DOC_DIR/
     echo "[$(date +%H:%M:%S)]✅ The documents were transferred to Documents/" >> $LOG_FILE
 else
     echo "[$(date +%H:%M:%S)]ℹ️ No new documents found." >> $LOG_FILE
 fi
 
-echo "Done! Check $LOG_FILE for details"
-echo "--- Finished  ---"
+echo "Done! Checking for old temporary files..."
+find . -type f -mtime +30 -name "*.tmp" -delete
+echo "[$(date +%H:%M:%S)] CLEANUP: Old .tmp file deleted" >> $LOG_FILE
+echo "Process completed! Chech $LOG_FILE for details"
