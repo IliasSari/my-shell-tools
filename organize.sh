0814972922047eba56331f68e9620b82d28a3c7d
IliasSari <sarigilias@gmail.com>
2026-03-15 17:17:47 +0200

    feat: add logging functionality with timestamps

--- organize.sh
+++ organize.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LOG_FILE="organize.log"
 
 IMG_DIR="Photos"
 DOC_DIR="Documents"
@@ -6,20 +7,24 @@ TXT_DIR="PlainTexts"
 
 mkdir -p $IMG_DIR $DOC_DIR $TXT_DIR
 
-echo "--- Start ---"
+echo "--- Start ---"  $LOG_FILE
+echo "Execution Date: $(date)">> $LOG_FILE
+
+echo "Starting organization..."
 
 if ls *.jpg *.png *.jpeg >/dev/null 2>&1; then
     mv *.jpg *.png *.jpeg $IMG_DIR/
-    echo "✅ The images were transferred to $IMG_DIR."
+    echo "✅ The images were transferred to $IMG_DIR." >> $LOG_FILE
 else
-    echo "ℹ️ No new images found."
+    echo "ℹ️ No new images found." >> $LOG_FILE
 fi
 
 if ls *.pdf *.docx >/dev/null 2>&1; then
-    mv *.pdf *.docx $DOC_DIR/
-    echo "✅ The documents were transferred to $DOC_DIR."
+    mv *.pdf *.docx Documents/
+    echo "[$(date +%H:%M:%S)]✅ The documents were transferred to Documents/" >> $LOG_FILE
 else
-    echo "ℹ️ No new documents found."
+    echo "[$(date +%H:%M:%S)]ℹ️ No new documents found." >> $LOG_FILE
 fi
 
+echo "Done! Check $LOG_FILE for details"
 echo "--- Finished  ---"
